28cb4170813910a46f516710e4c515a5d89f0f7e
EiriniMoroz <EiriniMoroz@users.noreply.github.com>
2022-07-08 09:12:31 +0000

    Ready to clone and code.

---
+++ android/app/src/main/kotlin/flutter_test_task_for_solid_software/flutter_test_task_for_solid_software/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutter_test_task_for_solid_software.flutter_test_task_for_solid_software
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
